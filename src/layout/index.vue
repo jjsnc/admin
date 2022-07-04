b58f65629bd05fc004735e2bd6ee8a9cf9a5d59e
卓文选 <zhuowangyi@163.com>
2022-07-04 11:47:43 +0800

    移动端检测

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,27 +1,103 @@
 
 <template>
   <div class="app-wrapper">
+    <div
+      v-if="device === 'mobile' && sidebar.opened"
+      class="drawer-bg"
+      @click="handleClickOutside"
+    />
     <sidebar class="sidebar-container" />
     <app-main></app-main>
   </div>
 </template>
 
+
 <script lang="ts">
 import { defineComponent } from 'vue'
 import { Sidebar, AppMain } from './components'
+import { mapState } from 'vuex'
+
 export default defineComponent({
+  data() {
+    return {
+      WIDTH: 992 // refer to Bootstrap's responsive design
+    }
+  },
   components: {
     Sidebar,
     AppMain
+  },
+
+  mounted() {
+    const isMobile = this.$_isMobile()
+    if (isMobile) {
+      this.$store.dispatch('app/toggleDevice', 'mobile')
+      this.$store.dispatch('app/closeSideBar', { withoutAnimation: true })
+    }
+  },
+  computed: {
+    ...mapState({
+      // @ts-ignore
+      device: (state) => state.app.device,
+      // @ts-ignore
+      sidebar: (state) => state.app.sidebar
+    })
+  },
+  beforeMount() {
+    window.addEventListener('resize', this.$_resizeHandler)
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.$_resizeHandler)
+  },
+  methods: {
+    handleClickOutside() {
+      this.$store.dispatch('app/closeSideBar', { withoutAnimation: false })
+    },
+    $_isMobile() {
+      let { WIDTH } = this
+      const rect = document.body.getBoundingClientRect()
+      const { width } = rect
+      return width - 1 < WIDTH
+    },
+    $_resizeHandler() {
+      if (!document.hidden) {
+        const isMobile = this.$_isMobile()
+        this.$store.dispatch('app/toggleDevice', isMobile ? 'mobile' : 'desktop')
+        if (isMobile) {
+          this.$store.dispatch('app/closeSideBar', { withoutAnimation: true })
+        }
+      }
+    }
+  },
+  watch: {
+    $route(route) {
+      if (this.device === 'mobile' && this.sidebar.opened) {
+        this.$store.dispatch('app/closeSideBar', { withoutAnimation: false })
+      }
+    }
   }
 })
 </script>
 
 
 
-<style>
-a {
-  display: block;
-  margin: 16px;
+<style lang="scss" scoped>
+@import '@/styles/mixin.scss';
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+
+.drawer-bg {
+  background: #000;
+  opacity: 0.3;
+  width: 100%;
+  top: 0;
+  height: 100%;
+  position: absolute;
+  z-index: 999;
 }
 </style>
+
